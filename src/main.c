5c5387eab6bd030dd28540bda35a8a97ba593530
hugoralf <ralf.hugo@gmail.com>
2017-08-03 00:19:17 -0300

    tarefa realizada

--- src/main.c
+++ src/main.c
@@ -4,19 +4,28 @@
  * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
  * numero de palavras separadas que recebeu e, apos, encerrar.
  */
-
+/* Hugo Ralf Pereira RA:155742*/
 #include <stdio.h>
 
 int main() {
+  /*declaracao de variaveis auxiliares e para recebimento do caractere*/
+  char c, aux = '\0', aux2 = '\0';
+  /*declaracao de variavel para o contador*/
+  int counter = 0;
+/*laco para verificacao e contagem de palavras*/
+  do{
+    scanf("%c", &c);    
+	if((c == '.' || c == ' ' || c == '\n' || c == ',' || c == '!' || c == '-') 
+		&& (aux == '.' || aux == ' ' || aux == ',' || aux == '!' || aux == '-')){}
+	else if((c == '.' || c == ',') && aux >= '0' && aux <= '9'){}
+	else if (c == ' ' || c == '\n' || c == ',' || c == '.' || c == '-' || c == '!' 
+		|| (c >= '0' && c  <= '9' && aux == '.' && aux2 >= '0' && aux2 <= '9'))
+		counter ++;
+	aux2 = aux;
+    aux = c;
+  }while(c != '\n');
 
-  char c;
-
-  c = 1;
-
-  while (c != '\n') {
-    scanf("%c", &c);
-  }
-
-  printf("1\n");
+	/*impressao do contador de palavras*/
+  printf("%d\n", counter);
   return 0;
 }
